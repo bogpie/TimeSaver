f5d0fc34345c6166f2c62c00ab762cad7e5e6c4f
Bogdan Piele <bogdanpielemail@gmail.com>
2021-11-11 18:28:43 +0200

    Initial commit

---
+++ entry/src/main/java/com/timesaver/timesaver/slice/MainAbilitySlice.java
@@ -0,0 +1,43 @@
+package com.timesaver.timesaver.slice;
+
+import com.timesaver.timesaver.ResourceTable;
+import ohos.aafwk.ability.AbilitySlice;
+import ohos.aafwk.content.Intent;
+import ohos.agp.components.Button;
+import ohos.agp.components.TimePicker;
+
+public class MainAbilitySlice extends AbilitySlice {
+    @Override
+    public void onStart(Intent intent) {
+        super.onStart(intent);
+        super.setUIContent(ResourceTable.Layout_ability_main);
+
+        Button buttonToBreaksSetup = (Button) findComponentById(ResourceTable.Id_buttonToBreaksSetup);
+//        buttonToBreaksSetup.setClickedListener(listener -> present(new BreaksSetupAbilitySlice(), new Intent()));
+
+        TimePicker timePicker = (TimePicker) findComponentById(ResourceTable.Id_time_picker);
+        timePicker.showSecond(false);
+        int hour = timePicker.getHour();
+        int minute = timePicker.getMinute();
+
+        timePicker.setHour(0);
+        timePicker.setMinute(0);
+        timePicker.setRange(new int[]{0, 0, 0, 12, 59, 59});
+
+        timePicker.setTimeChangedListener((timePicker1, hour1, minute1, second1) -> {
+                    //ToDo:
+                }
+        );
+
+    }
+
+    @Override
+    public void onActive() {
+        super.onActive();
+    }
+
+    @Override
+    public void onForeground(Intent intent) {
+        super.onForeground(intent);
+    }
+}
---
+++ entry/src/test/java/com/timesaver/timesaver/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.timesaver.timesaver;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
