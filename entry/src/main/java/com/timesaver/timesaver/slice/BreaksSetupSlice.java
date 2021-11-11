808e0ca06d294497f1ee924078ad72316dc20fd5
Bogdan Piele <bogdanpielemail@gmail.com>
2021-11-11 20:43:24 +0200

    Rebuilt app because of bug on previous project

--- entry/src/main/java/com/timesaver/timesaver/slice/BreaksSetupSlice.java
+++ entry/src/main/java/com/timesaver/timesaver/slice/BreaksSetupSlice.java
@@ -6,12 +6,25 @@ import ohos.aafwk.content.Intent;
 import ohos.agp.components.Button;
 
 public class BreaksSetupSlice extends AbilitySlice {
+    int hour = 0;
+    int minute = 0;
+
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_breaks_setup);
         Button buttonToStartTime = (Button) findComponentById(ResourceTable.Id_buttonToStartTime);
         buttonToStartTime.setClickedListener(listener -> present(new TimerSlice(), new Intent()));
+
+        if (getAbility() != null) {
+            if (getAbility().getIntent() != null) {
+                if (getAbility().getIntent().hasParameter("HOUR")) {
+                    hour = getAbility().getIntent().getIntParam("HOUR", 0);
+                    minute = getAbility().getIntent().getIntParam("MINUTE", 0);
+                }
+            }
+        }
 
     }
 
--- entry/src/main/java/com/timesaver/timesaver/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/timesaver/timesaver/slice/MainAbilitySlice.java
@@ -1,34 +1,59 @@
 package com.timesaver.timesaver.slice;
 
+import com.timesaver.timesaver.MainAbility;
 import com.timesaver.timesaver.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.aafwk.content.Operation;
 import ohos.agp.components.Button;
 import ohos.agp.components.TimePicker;
 
 public class MainAbilitySlice extends AbilitySlice {
+    int hour = 0;
+    int minute = 0;
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
 
         Button buttonToBreaksSetup = (Button) findComponentById(ResourceTable.Id_buttonToBreaksSetup);
-        buttonToBreaksSetup.setClickedListener(listener -> present(new BreaksSetupSlice(), new Intent()));
 
         TimePicker timePicker = (TimePicker) findComponentById(ResourceTable.Id_time_picker);
         timePicker.showSecond(false);
-        int hour = timePicker.getHour();
-        int minute = timePicker.getMinute();
 
         timePicker.setHour(0);
         timePicker.setMinute(0);
         timePicker.setRange(new int[]{0, 0, 0, 12, 59, 59});
 
-        timePicker.setTimeChangedListener((timePicker1, hour1, minute1, second1) -> {
-                    //ToDo:
+
+        timePicker.setTimeChangedListener((timepicker, hour, minute, second) -> {
+                    this.hour = hour;
+                    this.minute = minute;
                 }
         );
 
+
+        buttonToBreaksSetup.setClickedListener(listener -> {
+                    Intent passedIntent = new Intent();
+
+                    Operation operation =
+                            new Intent.OperationBuilder()
+                                    .withBundleName(getBundleName())
+                                    .withAbilityName(MainAbility.class.getName())
+                                    .withAction("action.setTime")
+                                    .build();
+
+                    passedIntent.setOperation(operation);
+
+                    passedIntent.setParam("HOUR", hour);
+                    passedIntent.setParam("MINUTE", hour);
+
+                    //startAbility(passedIntent);
+
+                    present(new BreaksSetupSlice(), passedIntent);
+                }
+        );
     }
 
     @Override
